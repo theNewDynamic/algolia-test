f6677d5c994f0cce6e6127c3c52ecc81ca7406cd
budparr <budparr@gmail.com>
2018-01-30 15:49:29 -0500

    Add as a script

--- static/algolia-index.js
+++ static/algolia-index.js
@@ -1,14 +1,13 @@
 var algoliasearch = require("algoliasearch")
 var fs = require("fs")
 
-
 var indexId = "testhugoagolia_8"
 var indexFile = fs.readFileSync("./public/index.json")
 var index = JSON.parse(indexFile)
 
 if (index) {
   var APP_ID = "Y5U3SE5SW2"
-  var ADMIN_KEY = process.env.ALGOLIA_WRITE_KEY;
+  var ADMIN_KEY = "c023a8822dda231cbc7a3921e16abba9"
   var index = algoliasearch(APP_ID, ADMIN_KEY).initIndex(indexId)
 
   index.addObjects(index, function(err, content) {
