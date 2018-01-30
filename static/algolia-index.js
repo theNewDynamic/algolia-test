e1d5f58d274521d092f835f9000d0de323a3b351
budparr <budparr@gmail.com>
2018-01-30 14:50:22 -0500

    [wip] Add separate script for holding

---
+++ static/algolia-index.js
@@ -0,0 +1,18 @@
+var algoliasearch = require("algoliasearch")
+var fs = require("fs")
+
+
+var indexId = "testhugoagolia_8"
+var indexFile = fs.readFileSync("./public/index.json")
+var index = JSON.parse(indexFile)
+
+if (index) {
+  var APP_ID = "Y5U3SE5SW2"
+  var ADMIN_KEY = process.env.ALGOLIA_WRITE_KEY;
+  var index = algoliasearch(APP_ID, ADMIN_KEY).initIndex(indexId)
+
+  index.addObjects(index, function(err, content) {
+    if (err) throw err
+    else console.log("Sending index to Algolia")
+  })
+}
